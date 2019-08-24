039b61e68818cbafe694b3a51b274d84e38beb68
Brady Webb <Bradywebb@Bradys-MacBook-Pro-2.local>
2019-08-23 19:01:12 -0500

    added features

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Brady Webb on 8/23/19.
+//  Copyright © 2019 Brady Webb. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
